a8aec7855a5b5472ee9e57921ded938fb167afbb
Harsh-k17 <harshkhandagale17@gmail.com>
2025-11-24 19:07:14 +0530

    Add files via upload

---
+++ FirstProgram.py
@@ -0,0 +1,33 @@
+'''
+Hello Viewers,
+Its a first program of a python
+
+If you want to display a group of character in monitor.so I can use the print () Function 
+We use double quotes and single quotes to deisplay a group of Character.
+
+When the print function is called simply,it will throw the cursor to the next line it means that blank line will be displayed.
+
+'''
+
+
+# python program to use print Function
+
+print("Hello Guys!")
+
+print('Today is the positive day..')
+
+#print Variables list Statment
+
+a,b = 2,3 # a is a variable and 2 is the value 
+
+print(a) #without quotes we dsiplay a value to Stored by a variable a.
+
+print(b)
+
+print(a,b,sep=',') #Values in the output are seprated by a space by default.to separate the output with comma
+
+print(a,b,sep=':') #Output with Colon
+
+print(a,b,sep='-----') # output wih hyphen
+
+
