c63c79e4605a183451234ad3ace57ed080b687e9
Harsh Khandagale <harshkhandagale17@gmail.com>
2025-11-26 20:05:45 +0530

    Add files via upload

---
+++ count.py
@@ -0,0 +1,31 @@
+# A python program to count number of lines , words and characters in a text file
+
+#couting number of lines, words and characters in a text file
+
+import os,sys
+
+#open the file for reading data
+fname = input('Enter filename: ')
+
+if os.path.isfile(fname):
+    f = open(fname,'r')
+else:
+    print(fname+'does not exist')
+    sys.exit()
+
+#initialize the counters to 0
+cl = cw = cc = 0
+
+#read line by line from the file
+for line in f:
+    words = line.split()
+    cl+=1
+    cw+=len(words)
+    cc+=len(line)
+
+print('No of lines: ',cl)
+print('No of words ',cw)
+print('No of characters ',cc)
+
+#close the file
+f.close()
